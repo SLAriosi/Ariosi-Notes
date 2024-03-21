00f4018205ec3e88a5d83ff9bee5966549a0b9c7
Ariosi Lucas <ariosilucas@gmail.com>
2024-03-21 17:13:27 -0300

    CSS in JS

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,5 +1,6 @@
 //Importando os estilos para serem usados aqui no index. Você não precisa fazer nada dentro do arquivo style.
-import "./style.css";
+import { Container } from "./styles.js";
+// Para Utilizar o estilo Container que foi Importado do CSS temos que colocar Container dentro dos Fragments aonde queremos modificar o estilo.
 
 //Quando se exporta usando a linha 1 de exemplo, você deve desestruturar a variável dentro do main.jsx no import na linha 3, e você desestrutura no formato de objeto, já que função vira um objeto, (Você passa os valores da função dentro de {} oque significa objeto.)
 export function Details() {
@@ -10,9 +11,10 @@ export function Details() {
     // <> </> = Fragment
     // O fragment serve para podermos colocar dentro de um return vários tipos de elementos
     // Se preferir você também pode usar <div> </div> no lugar do Fragment, a diferença é que com a div você consegue estilizar as coisas já o fragmento não.
-    <>
+    <Container>
       <h1>Hello World!</h1>
       <span>Lucas Ariosi</span>
-    </>
+    </Container>
+    // Aqui utilizo o Container dentro do Fragment para puxarmos o estilo Container que puxamos do style.js
   )
 }
---
+++ src/pages/Details/styles.js
@@ -0,0 +1,7 @@
+//Para iniciar os estilos em formato JavaScript precisa puxar a informação
+import styled from "styled-components";
+
+//para puxar esse estilo do Container utilizasse a linha import { Container } from "./style.js"
+export const Container = styled.div`
+   background: red;
+`;
