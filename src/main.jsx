61409babdef36a3405c6b60d65b1740b2b5ec380
Ariosi Lucas <ariosilucas@gmail.com>
2024-03-21 15:54:09 -0300

    JSX how to use and create an Interface

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+
+//Se desestrutura o Details { Details } dessa maneira pois lá em Details.jsx ele não é um export default, portanto devesse DESESTRUTURAR a função Details(){}, E como uma função recebe seus valores em formato de OBJETO ("{}") é necessário desestruturar com {} aqui no inicio do programa
+import { Details } from './pages/Details'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Details />
   </React.StrictMode>,
 )
---
+++ src/pages/Details.jsx
@@ -0,0 +1,9 @@
+export function Details() {
+  
+  return(
+    <h1>Hello World!</h1>
+  )
+}
+
+//Quando se exporta usando a linha 1 de exemplo, você deve desestruturar a variável dentro do main.jsx no import na linha 3, e você desestrutura no formato de objeto, já que função vira um objeto, (Você passa os valores da função dentro de {} oque significa objeto.)
+
