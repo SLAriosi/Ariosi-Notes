60a7c2560740725d56fcba065825dd26bf88a1cf
Ariosi Lucas <ariosilucas@gmail.com>
2024-03-21 16:33:43 -0300

    Adding CSS

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,3 +1,6 @@
+//Importando os estilos para serem usados aqui no index. Você não precisa fazer nada dentro do arquivo style.
+import "./style.css";
+
 //Quando se exporta usando a linha 1 de exemplo, você deve desestruturar a variável dentro do main.jsx no import na linha 3, e você desestrutura no formato de objeto, já que função vira um objeto, (Você passa os valores da função dentro de {} oque significa objeto.)
 export function Details() {
 
