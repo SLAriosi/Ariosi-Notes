4c4f7e8942fc0a7b02205b1c8afad934ec13dc0b
Ariosi Lucas <ariosilucas@gmail.com>
2024-03-22 08:30:45 -0300

    Global Theme, and doing the documentation of the big most os the things

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,21 @@
+// Importando as bibliotecas do react necessárias para a utilização dele
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-//Se desestrutura o Details { Details } dessa maneira pois lá em Details.jsx ele não é um export default, portanto devesse DESESTRUTURAR a função Details(){}, E como uma função recebe seus valores em formato de OBJETO ("{}") é necessário desestruturar com {} aqui no inicio do programa
+// Importando o styled-components da extesão styled-components (unha sendo pintada) 
+import { ThemeProvider } from 'styled-components'
+
+// Se desestrutura o Details { Details } dessa maneira pois lá em Details.jsx ele não é um export default, portanto devesse DESESTRUTURAR a função Details(){}, E como uma função recebe seus valores em formato de OBJETO ("{}") é necessário desestruturar com {} aqui no inicio do programa
 import { Details } from './pages/Details'
 
+// Importando esse tema e colocando na linha +- 13 Onde iremos passar o tema padrão do estilo CSS.
+import theme from './styles/theme'
 
+// Corpo em estilo HTML usando React no getElementByID nós escrevemos a classe que será colocada esse conteúdo no index.html, nesse caso, e padronizado, vem root. você pode observar essa classe indo até o index.html provavelmente na linha 9 do index.html e lá que será inserido o conteúdo listado aqui abaixo. 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Details />
+    <ThemeProvider theme={theme}>
+      <Details />
+    </ThemeProvider>
   </React.StrictMode>,
 )
---
+++ src/styles/theme.js
@@ -0,0 +1,17 @@
+// Aqui criaremos os estilos globais, que todos os arquivos terão em comum.
+export default {
+   COLORS: {
+      BACKGORUND_900: "#232129",
+      BACKGORUND_800: "#312E38",
+      BACKGORUND_700: "#3E3B47",
+
+   WHITE: "#F4EDE8",
+   ORANGE: "#FF9000",
+
+   GRAY_100: "#999591",
+   GRAY_300: "#666360",
+   
+   RED: "#FF002E",
+
+   }
+}
