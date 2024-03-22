e2170e0e482b83c9dc3a1c4cc53e127e31fcda38
Ariosi Lucas <ariosilucas@gmail.com>
2024-03-22 14:53:41 -0300

    Properties How to use

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -2,12 +2,27 @@
 import { Container } from "./styles";
 
 //O código selecionado é um componente funcional React que exporta um componente Button.
-export function Button() {
+// Caso você queira fazer esse container ficar de uma maneira que você possa reaproveita-lo no futuro, você adiciona props dentro do parâmetro da função que você está ativando.
+// Caso queiramos passar mais propriedades só precisamos por , com a nova propriedade necessária.
+// Se o seu botão estiver com várias propriedades você pode usar o rest operator, que é o ...rest caso você não tenha informado as propriedades novas como o loading, ou o title logo de início, você irá inserir as novas propriedades que colocou lá no index do Details dentro desse ...rest.
+// A estratégia do Rest é simplesmente ter o pensamento de que, se você não quer passar todas as propriedades porque um botão tem muitas propriedades, você apenas passa esse ...rest, e as adiciona direto dentro dele.
+// Porque passar o loading = false, simplesmente porque caso você não tenha definido ele direto no botão, é bom deixá-lo padronizado como falso.
+export function Button({title, loading = false, ...rest}) {
 
    // Tem que ter esse return para retornar de maneira automática depois de ser chamada essa função Button
    return(
-   <Container type="button">
-      Botão Normal
+   
+   // Nós utilizamos esse title, para quando quisermos trocar oque tem dentro do botão nós apenas irmos para o botão em específico e colocarmos title="valor desejado" caso seja numero coloque title={10}
+   // Porque é title, apenas pra ficar organizado, mas é ele quem muda o nome, apenas porque ele faz a mesma função de Botão e tudo que está dentro de uma variavel Botão se torna o texto do Botão, isso aqui passa pelo mesmo principio.
+   <Container 
+      type="button"
+      disabled={loading}
+      {...rest}
+   >
+      { loading ? "Carregando..." : title}
    </Container>
+   // Explicação Loading: Caso o Loading seja verdadeiro, então irá desabilitar o botão, assim como passamos o loading como uma função Booleana lá no index.jsx do Details (./src/pages/Details/index.jsx)
+   // Explicando if ternário: ele é separado dessa forma { "nessa parte vai oque você quer ver se é verdadeiro / falso, caso seja verdadeiro acontece esse próximo primeiro caso e caso falso acontece oque está descrito depois dos dois pontos ':' " ? "Verdadeiro tá certo" : "Falso tá errado" }
+   // Continuando Explicação: Podemos passar também atributos dentro dessas 3 partes do if ternário, não precisa ser necessáriamente strings nem números.
    )
 }
--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -3,7 +3,7 @@ import { Container } from "./styles.js";
 // Para Utilizar o estilo Container que foi Importado do CSS temos que colocar Container dentro dos Fragments aonde queremos modificar o estilo.
 
 // Importamos o component Button da pasta componentes para que nosso index consiga enxergar dentro daquela pasta e utilizar aquela estilização de botão.
-import { Button } from '../../components/Button' 
+import { Button } from '../../components/Button'
 
 //Quando se exporta usando a linha 1 de exemplo, você deve desestruturar a variável dentro do main.jsx no import na linha 3, e você desestrutura no formato de objeto, já que função vira um objeto, (Você passa os valores da função dentro de {} oque significa objeto.)
 export function Details() {
@@ -14,13 +14,19 @@ export function Details() {
     // <> </> = Fragment
     // O fragment serve para podermos colocar dentro de um return vários tipos de elementos
     // Se preferir você também pode usar <div> </div> no lugar do Fragment, a diferença é que com a div você consegue estilizar as coisas já o fragmento não.
+    // Dentro dos Buttons utilizamos a tag title ou label ou name, você quem escolhe qual usar, mas ela é uma propriedade que depois iremos recuperar dentro do seu component que nesse caso fica na ./src/components/Button/index.jsx lá dentro eu explico mais no local correto. "linha 5"
+    // Na propriedade title para você atribuir um novo valor caso seja texto precisa apenas escrever o title="texto" e caso seja numeros você escreve title={10} por exemplo.
     <Container>
       <h1>Hello World!</h1>
       <span>Lucas Ariosi</span>
 
-      <Button />
+      <Button title="Loading" />
+      <Button title="Cadastrar" />
+      <Button title="Voltar" />
     </Container>
+    // Para adicionar propriedades não precisa fazer nada além de dar um espaço, e caso queira adicionar uma propriedade booleana, você só precisa passar a propriedade sem chaves ou aspas.
     // Aqui utilizo o Container dentro do Fragment para puxarmos o estilo Container que puxamos do style.js
+    // Agora caso queiramos usar o loading   
   )
 }
 
