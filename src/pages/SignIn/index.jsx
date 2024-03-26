2410b4b91bea6635cef6ac1189dca2023163eeb5
Ariosi Lucas <ariosilucas@gmail.com>
2024-03-26 08:55:58 -0300

    Form of the SignIn page

--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -1,9 +1,46 @@
-import { Container } from './styles'
+// Importar o Container de dentro do styles aqui da página SignIn.
+// Form também vai vir dessa mesma pasta styles, por isso importamos assim com vírgula.
+import { Container, Form } from './styles'
 
+// Agora importamos o icon que iremos utilizar dentro do Input, importamos de dentro da biblioteca do react-icons, e temos que selecionar a biblioteca que iremos utilizar de icones também que nesse caso é a "fi", para descobrir a biblioteca do ícone que você deseja utilizar, entre no site do react icons.
+import { FiMail, FiLogIn, FiLock } from 'react-icons/fi'
+
+// Importando o Input pois iremos utilizar isso dentro do formulário (CLARAMENTE!! dskldsldksld).
+import { Input } from '../../components/Input'
+
+// Importando o button para utilizarmos como um componente de Botão para o formulário, para confirmar e entrar na aplicação
+import { Button } from '../../components/Button'
+
+// Dentro do estilo principal aqui do index.jsx, colocamos o form, vamos criar o form aí dentro desse componente.
 export function SignIn() {
    return (
       <Container>
-         
+         <Form>
+            <h1>Ariosi Notes</h1>
+            
+            <p>Aplicação para salvar e gerenciar seus links úteis</p>
+
+            <h2>Faça seu login</h2>
+
+            <Input
+               placeholder="E-mail"
+               type="text"
+               icon={FiMail}
+            />
+
+            <Input
+               placeholder="Senha"
+               type="password"
+               icon={FiLock}
+            />
+
+            <Button title={"Entrar"}/>
+
+            <a href="#">
+               Criar conta
+            </a>
+
+         </Form>
       </Container>
    )
 }
--- src/pages/SignIn/styles.js
+++ src/pages/SignIn/styles.js
@@ -3,6 +3,39 @@ import styled from "styled-components";
 // Exportando o Container (nome que atribuimos como padrão, pode ser outro), puxando o styled do styled-components, e definindo esse container como sendo uma div.
 export const Container = styled.div`
    height: 100vh;
+
    display: flex;
    align-items: stretch;
+`;
+
+export const Form = styled.form`
+   padding: 0 13.6rem;
+
+   display: flex;
+   flex-direction: column;
+   justify-content: center;
+   align-items: center;
+
+   text-align: center;
+
+   > h1 {
+      font-size: 4.8rem;
+      color: ${({ theme }) => theme.COLORS.ORANGE};
+   }
+   
+   > h2 {
+      font-size: 2.4rem;
+      margin-top: 8.4rem;
+      margin-bottom: 2.4rem;
+   }
+   
+   > p {
+      font-size: 1.4rem;
+      color: ${({ theme }) => theme.COLORS.GRAY_100};
+   }
+
+   > a {
+      margin-top: 12.4rem;
+      color: ${({ theme }) => theme.COLORS.ORANGE};
+   }
 `;
