2e2f5889e946e151f97ccfaa08df52054a4b8f95
Ariosi Lucas <ariosilucas@gmail.com>
2024-04-02 11:10:50 -0300

    Using Local Storage To Bring the Token and User, and The AuthRoute dont be needed now, only if there is not a token or user in the LocalStorage.

--- src/hooks/auth.jsx
+++ src/hooks/auth.jsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useState, useEffect } from "react";
 
 //Como precisamos passar a informação do signIn lá na linha +-12 para o back-end então nós importamos a nossa API
 import { api } from "../services/api"
@@ -15,6 +15,13 @@ function AuthProvider({ children }) {
          const response = await api.post("/sessions", { email, password })
          const { user, token } = response.data
 
+         // Aqui passamos que o usuário será guardado dentro do LocalStorage, dentro da máquina do usuário, então passamos o nome da aplicação, é um padrão que adotamos, e depois passamos o item que queremos puxar, como o localStorage só consegue armazenar objetos de texto / Strings, então temos que alterar o tipo de item que é o usuário, já que ele é um objeto podemos usar a própria função do JSON a .stringify() para transformar esse objeto que é o usuário em uma string.
+         // Usamos o JSON.stringify para transformar alguma variável do tipo objeto em um arquivo String feito em formato Json. Nesse caso ela só converte o tipo da variável.
+         localStorage.setItem("@ariosinotes:user", JSON.stringify(user))
+
+         // Também setamos para guardar o token na máquina do Usuário, usando a mesma estrutura, e como o token já é uma string, não precisamos usar o JSON.stringify
+         localStorage.setItem("@ariosinotes:token", token)
+
          api.defaults.headers.authorization = `Bearer ${token}`
          setData({user, token})
          
@@ -26,6 +33,28 @@ function AuthProvider({ children }) {
          }
       }
    }
+
+   // Como funciona o Use Effect, Ele tem 2 partes a primeira parte é a função que eu quero que ele execute, ele sempre vai executar após o carregamento do componente, e na segunda parte é um vetor que você pode colocar um estado, e caso esse estado mude, o useEffect executa novamente
+   useEffect(() => {
+      
+      // Agora vamos pegar o valor que queremos para quando abrir a página ela abrir. Para obter essas informações usamos o .getItem, e passamos exatamente a mesma tag que usamos para guardar a informação
+      const token = localStorage.getItem("@ariosinotes:token")
+      const user = localStorage.getItem("@ariosinotes:user")
+
+      // Agora para ter certeza de que temos ambos o usuário e o token salvos na LocalStorage, para que aí sim mudemos a rota de início, usamos a validação abaixo.
+      if (token && user) {
+
+         // Essa linha insere o Token dentro do Cabeçalho (headers)
+         api.defaults.headers.authorization = `Bearer ${token}`
+
+         // Por final setamos os dados que queremos, e como o usuário é um objeto, temos que usar o JSON.parse que transforma de volta o usuário que antes transformamos em um formato String para o tipo JSON.
+         setData({
+            token,
+            user: JSON.parse(user)
+         })
+      }
+
+   }, [])
 
    return (
       <AuthContext.Provider value={{ signIn, user: data.user }}>
