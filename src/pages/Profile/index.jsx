b3104f98710f27d4c3e69440058ebe94f123fcd5
Ariosi Lucas <ariosilucas@gmail.com>
2024-04-02 14:09:40 -0300

    Uploading user Data, in the Page Profile

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -1,14 +1,24 @@
+import { useState } from "react";
 import { FiArrowLeft, FiMail, FiUser, FiLock, FiCamera } from "react-icons/fi";
+import { Link } from "react-router-dom";
+
+import { useAuth } from "../../hooks/auth"
+
+import { Input } from "../../components/Input"
+import { Button } from "../../components/Button"
 
 import { Container, Form, Avatar } from "./styles";
 
-import { Input } from "../../components/Input"
-
-import { Button } from "../../components/Button"
-
-import { Link } from "react-router-dom";
-
 export function Profile() {
+   
+   const { user } = useAuth()
+   
+   // De prache colocar os estados no começo da função
+   const [name, setName] = useState(user.name)
+   const [email, setEmail] = useState(user.email)
+   const [passwordOld, setPasswordOld] = useState()
+   const [passwordNew, setPasswordNew] = useState()
+
    return (
       <Container>
          <header>
@@ -38,25 +48,31 @@ export function Profile() {
                placeholder="Nome"
                type="text"
                icon={FiUser}
-            />
+               value={name}
+               onChange={e => setName(e.target.value)}
+               />
 
             <Input
                placeholder="E-mail"
                type="text"
                icon={FiMail}
-            />
+               value={email}
+               onChange={e => setEmail(e.target.value)}
+               />
 
             <Input
                placeholder="Senha atual"
                type="password"
                icon={FiLock}
-            />
+               onChange={e => setPasswordOld(e.target.value)}
+               />
 
             <Input
                placeholder="Nova Senha"
                type="password"
                icon={FiLock}
-            />
+               onChange={e => setPasswordNew(e.target.value)}
+               />
 
             <Button title={"Salvar"} />
 
