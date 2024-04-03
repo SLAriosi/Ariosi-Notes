7f8235f8025d9964067b5e2d7c148a221c8fde12
Ariosi Lucas <ariosilucas@gmail.com>
2024-04-03 17:04:22 -0300

    Uploading Tags to the page Home

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,3 +1,7 @@
+// Utilizamos o useState para que possamos criar um estado dentro da função da Home.
+// E o useEffect é uma funçao que é executada de forma automática quando nossa interface é renderizada
+import { useState, useEffect } from 'react';
+
 // Importando todos os Itens que iremos atualizar no styles.js.
 import { Container, Brand, Menu, Search, Content, NewNote } from './styles'
 
@@ -9,18 +13,26 @@ import { Header } from '../../components/Header'
 // Importando as notas, elas existem dentro de uma "Section" e lá é onde passamos os parâmetros corretos para cada nota e oque queremos colocar nelas.
 // Passamos o id e o nome que queremos das às tags, como parâmetros e objetos dentro delas (dentro da SECTION).
 import { Note } from '../../components/Note'
-
-
 import { Input } from '../../components/Input'
 
 //Importamos a Seção pois as "Note" estão dentro de uma seção 
 import { Section } from '../../components/Section'
 
 import { ButtonText } from '../../components/ButtonText'
-// import { Header } from '../../components/Header'
-// import { Header } from '../../components/Header'
-// import { Header } from '../../components/Header'
+import { api } from '../../services/api';
+
 export function Home() {
+   const [tags, setTags] = useState([]);
+
+   useEffect(() => {
+      async function fetchTags() {
+         const response = await api.get("/tags")
+         setTags(response.data)
+      }
+
+      fetchTags();
+   }, []);
+
    return (
       <Container>
 
@@ -34,9 +46,18 @@ export function Home() {
 
          <Menu>
 
-            <li><ButtonText title={"Todos"}  /></li>
-            <li><ButtonText title={"React"} isActive /></li>
-            <li><ButtonText title={"NodeJs"} isActive /></li>
+            <li><ButtonText title={"Todos"} isActive /></li>
+
+            {
+               tags && tags.map(tag => (
+                  <li key={String(tag.id)}>
+                     <ButtonText
+                        title={tag.name}
+                     />
+                  </li>
+               ))
+
+            }
 
          </Menu>
 
@@ -60,9 +81,9 @@ export function Home() {
             </Section>
 
          </Content>
-               
-               {/* Conseguimos usar o to para linkarmos esse botão com a rota New porque dentro do styles.js aqui da página Home, nós setamos ele como um styled(Link) explicações sobre o link abaixo. */}
-               {/* O link é importado do react-router-dom e utilizado como um padronizador, que facilita a navegação (ele deixa mais fácil na hora de mudar a rota do navegador "a URL com o /nomequevocequiser") */}
+
+         {/* Conseguimos usar o to para linkarmos esse botão com a rota New porque dentro do styles.js aqui da página Home, nós setamos ele como um styled(Link) explicações sobre o link abaixo. */}
+         {/* O link é importado do react-router-dom e utilizado como um padronizador, que facilita a navegação (ele deixa mais fácil na hora de mudar a rota do navegador "a URL com o /nomequevocequiser") */}
          <NewNote to="/New">
 
             <FiPlus />
