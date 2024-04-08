126b5e23d56459e8d0a10ae8c88661e8fbf5ad0b
Ariosi Lucas <ariosilucas@gmail.com>
2024-04-08 08:13:19 -0300

    Fixing other things

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const api = axios.create({
-   baseURL: "https://ariosinotes-api-290965fdcf66.herokuapp.comlocalhost:3333/"
+   baseURL: "https://ariosinotes-api-290965fdcf66.herokuapp.comlocalhost:3333"
 })
